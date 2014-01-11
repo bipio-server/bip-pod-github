f266dd39058e5646a70710630ce0b56d870a11dc
michael pearson <gravatar@x0rz.com>
2014-01-11 01:04:25 -0500

    create issue stub

--- README.md
+++ README.md
@@ -1,4 +1,34 @@
-bip-pod-github
-==============
+![Github](github.png) bip-pod-github
+=======
 
-Github Pod for BipIO
+<a href="https://www.github.com">github</a> pod for [bipio](https://bip.io).  
+
+## Installation
+
+From bipio server root directory
+
+    npm install bip-pod-github
+    ./tools/pod-install.js -a github [-u optional account-wide channel auto install]
+
+The pod-install script is a server script which will register the pod with the bipio server and add sparse
+configuration to your NODE_ENV environment config ('default.json', staging or production)
+keyed to 'github', based on the default config in the pod constructor.  It will also move the
+pod icon into the server cdn
+
+Manually restart the bipio server at your convenience.
+
+## Documentation
+
+[Bipio Docs](https://bip.io/docs/pods/github)
+
+## License
+
+BipIO is free for non-commercial use - [GPLv3](http://www.gnu.org/copyleft/gpl.html)
+
+Our open source license is the appropriate option if you are creating an open source application under a license compatible with the GNU GPL license v3. 
+
+Bipio may not be used for Commercial purposes by an entity who has not secured a Bipio Commercial OEM License.  To secure a Commercial OEM License for Bipio,
+please [reach us](mailto:enquiries@cloudspark.com.au)
+
+![Cloud Spark](http://www.cloudspark.com.au/cdn/static/img/cs_logo.png "Cloud Spark - Rapid Web Stacks Built Beautifully")
+Copyright (c) 2010-2013  [CloudSpark pty ltd](http://www.cloudspark.com.au)
---
+++ index.js
@@ -0,0 +1,45 @@
+/**
+ *
+ * The Bipio Github Pod.  Github Actions and Content Emitters
+ *
+ * @author Michael Pearson <michael@cloudspark.com.au>
+ * Copyright (c) 2010-2013 CloudSpark pty ltd http://www.cloudspark.com.au
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+var Pod = require('bip-pod'),
+Github = new Pod({
+  name : 'github',
+  description : 'Github',
+  description_long : 'GitHub is a web-based hosting service for software development projects that use the Git revision control system',
+  authType : 'oauth',
+  passportStrategy : require('passport-github').Strategy,
+  config : {
+    "oauth": {
+      "clientID" : "",
+      "clientSecret" : "",
+      "callbackURL" : "",
+      "scopes" : [
+      "gist",
+      "notifications",
+      "repo:status",
+      ]
+    }
+  }
+});
+
+Github.add(require('./issue_create.js'));
+
+// -----------------------------------------------------------------------------
+module.exports = Github;
